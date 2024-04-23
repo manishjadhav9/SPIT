1e68d657ec1dc2407d8cf3a681b3983d430378e9
Manish Jadhav <manishsj289@gmail.com>
2024-04-23 10:33:32 +0530

    evalution pre-testing

--- 4th SEM/DAA/Exp3/minmax.c
+++ 4th SEM/DAA/Exp3/minmax.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 #define ARRAY_SIZE 100000
-// #define ARRAY_SIZE 10
 // Function prototypes
 void generateNumbers(int numbers[], int size);
-void minMaxDivideConquer(int numbers[], int start, int end, int *min, int *max);
+void minMaxDivideConquer(int numbers[], int start, int end,
+                         int *min, int *max);
 void minMaxNaive(int numbers[], int size, int *min, int *max);
 int main()
 {
     FILE *p = fopen("minmax.csv", "w");
-    fprintf(p, "Number, Time (Divide & Conquer), Time (Naive), Min, Max\n");
+    fprintf(p, "Number, Time (Divide & Conquer), Time (Naive), Min, Max\n"); 
 
-     // Hard-coded array with 10 values
+    // Hard-coded array with 10 values
     // int numbers[ARRAY_SIZE] = {54, 21, 87, 63, 91, 37, 20, 48, 66, 15};
-
-    int numbers[ARRAY_SIZE];
-    int min_dc, max_dc, min_naive, max_naive;
-    //Generate 100,000 random integer numbers using rand()
-    generateNumbers(numbers, ARRAY_SIZE);
-    printf("Number, Time (Divide & Conquer), Time (Naive), Min, Max\n");
-    for (int i = 10; i <= ARRAY_SIZE; i += 10)
+    
+    int numbers[ARRAY_SIZE]; 
+    int min_dc, max_dc, min_naive, max_naive; 
+    // Generate 100,000 random integer numbers using rand() 
+    generateNumbers(numbers, ARRAY_SIZE); 
+    printf("Number, Time (Divide & Conquer), Time (Naive), Min, Max\n"); 
+    for (int i = 100; i <= ARRAY_SIZE; i += 100) 
     {
         clock_t start, end;
 
@@ -28,15 +28,15 @@ int main()
         start = clock();
         minMaxDivideConquer(numbers, 0, i - 1, &min_dc, &max_dc);
         end = clock();
-        double time_dc = ((double)(end - start)) / CLOCKS_PER_SEC;
+        double time_dc = ((double)(end - start)) /CLOCKS_PER_SEC;
         // Naive Approach
         start = clock();
         minMaxNaive(numbers, i, &min_naive, &max_naive);
         end = clock();
-        double time_naive = ((double)(end - start)) / CLOCKS_PER_SEC;
-        printf("%d, %lf, %lf, %d, %d\n", i, time_dc, time_naive, min_dc, max_dc);
-        fprintf(p, "%d, %lf, %lf, %d, %d\n", i, time_dc, time_naive, min_dc, max_dc);
-    }
+        double time_naive = ((double)(end - start)) /CLOCKS_PER_SEC;
+        printf("%d, %lf, %lf, %d, %d\n", i, time_dc,time_naive, min_dc, max_dc);
+        fprintf(p, "%d, %lf, %lf, %d, %d\n", i, time_dc,time_naive, min_dc, max_dc); 
+    } 
     return 0;
 }
 void generateNumbers(int numbers[], int size)
@@ -55,8 +55,8 @@ void minMaxDivideConquer(int numbers[], int start, int end, int *min, int *max)
     }
     int mid = (start + end) / 2;
     int min_left, max_left, min_right, max_right;
-    minMaxDivideConquer(numbers, start, mid, &min_left, &max_left);
-    minMaxDivideConquer(numbers, mid + 1, end, &min_right, &max_right);
+    minMaxDivideConquer(numbers, start, mid, &min_left,&max_left);
+    minMaxDivideConquer(numbers, mid + 1, end, &min_right,&max_right);
     *min = (min_left < min_right) ? min_left : min_right;
     *max = (max_left > max_right) ? max_left : max_right;
 }
@@ -75,3 +75,4 @@ void minMaxNaive(int numbers[], int size, int *min, int *max)
         }
     }
 }
+
